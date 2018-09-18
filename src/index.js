668b92d5bcd80c0c97551f3cf9a1f969e7704bb8
tania <masiukevich@gmail.com>
2018-09-18 16:29:36 +0300

    implemented getZeiosCount first attempt

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function getZerosCount(number, base) {
   // your implementation
+    //var end = base;
+    for (var i = 2; i < base; i++) {
+        if (base % i == 0) {
+            base /= i;
+            i--;
+        }
+    }
+    var result = 0;
+    while (number > 0) {
+        number /= base;
+        result += Math.floor(number);
+    }
+    return result;
 }
