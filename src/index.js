e3c634a44e4fdfe766a4be221886d74828b08f1d
tania <masiukevich@gmail.com>
2018-09-21 17:04:25 +0300

    implemented getZeiosCount first attempt

--- src/index.js
+++ src/index.js
@@ -1,16 +1,59 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
-    //var end = base;
-    for (var i = 2; i < base; i++) {
-        if (base % i == 0) {
-            base /= i;
-            i--;
+
+   var baseCopy = base;
+
+
+
+    var dividers = {}
+
+    var i = 2;
+
+
+
+    while (baseCopy > 1) {
+
+        if (baseCopy % i == 0) {
+
+            baseCopy /= i;
+
+            dividers[i] = dividers[i] ? dividers[i] + 1 : 1;
+
+            console.warn(i);
+
+        } else {
+
+            i++;
+
         }
+
     }
-    var result = 0;
-    while (number > 0) {
-        number /= base;
-        result += Math.floor(number);
+
+    //console.warn('all dividers are',  dividers);
+
+
+
+    var allResults = []
+
+    for (let divider of Object.keys(dividers)) {
+
+        var numberCopy = number;
+
+        let resultForDivider = 0;
+
+        while (numberCopy > 0) {
+
+            numberCopy /= (divider);
+
+            resultForDivider += Math.floor(numberCopy);
+
+        }
+
+        allResults.push(Math.floor(resultForDivider / dividers[divider]));
+
     }
+
+    var result = Math.min(...allResults);
+
     return result;
+
 }
